c94263ab2976b8993c3367557a3f5dce894f7399
codewouter <wpc_okhuijsen@hotmail.com>
2020-06-07 15:53:44 +0000

    fill all html enemies with stats. Fill hero stats. Adjust some aligning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,25 @@
-let orc = {
+var orc = {
     race:"Orc",
     maxHP: 30,
     currentHP: 15,
     attackDamage: 10
 }
 
-let hero = {
+var hero = {
     maxHP: 50,
     currentHP: 50,
     attackDamage: 20
 }
 
-let name = $("#enemyOne").html();
-console.log(name);
 $("#enemyOne").html(orc.race);
-$("#enemyOneMaxHP").html("MHP: " + orc.maxHP);
-$("#enemyOneCurrentHP").html("CHP: " + orc.currentHP);
+$("#enemyOneMaxHP").html("Max HP: " + orc.maxHP);
+$("#enemyOneCurrentHP").html("Current HP: " + orc.currentHP);
+
+$("#enemyTwo").html(orc.race);
+$("#enemyTwoMaxHP").html("Max HP: " + orc.maxHP);
+$("#enemyTwoCurrentHP").html("Current HP: " + orc.currentHP);
+
+$("#enemyThree").html(orc.race);
+$("#enemyThreeMaxHP").html("Max HP: " + orc.maxHP);
+$("#enemyThreeCurrentHP").html("Current HP: " + orc.currentHP);
 
