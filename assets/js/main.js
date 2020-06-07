050e2691c1ca1152e3902ed9690522a78b61191b
codewouter <wpc_okhuijsen@hotmail.com>
2020-06-07 17:34:56 +0000

    add shake function from bradleyhamilton &  https://jsfiddle.net/macloo/g39k3h3e/

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,10 +23,30 @@ $("#enemyThree").html(orc.race);
 $("#enemyThreeMaxHP").html("Max HP: " + orc.maxHP);
 $("#enemyThreeCurrentHP").html("Current HP: " + orc.currentHP);
 
+// adaptable SHAKE function, from 
+// https://bradleyhamilton.com/projects/shake/index.html 
+function shake(thing) {
+  var interval = 100;
+  var distance = 10;
+  var times = 6;
+
+  for (var i = 0; i < (times + 1); i++) {
+    $(thing).animate({
+      left:
+        (i % 2 == 0 ? distance : distance * -1)
+    }, interval);
+  }
+  $(thing).animate({
+    left: 0,
+    top: 0
+  }, interval);
+}
+
 function attack () {
+    shake($('#enemyOneImage'));
     orc.currentHP -= 10;
     console.log('attack!');
-$("#enemyOneCurrentHP").html("Current HP: " + orc.currentHP);
+    $("#enemyOneCurrentHP").html("Current HP: " + orc.currentHP);
 }
 
 $("#attackButton").click(function() {
